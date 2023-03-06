4fb522c9d8cb6d737a785b400505764a9850b355
Skylar Thompson <skylar2@uw.edu>
2023-03-06 11:26:23 -0800

    package management re #1

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,16 @@
 #
 # @example
 #   include spectrum_protect_client
-class spectrum_protect_client {
+# @param packages [Hash] Hash of package resource definitions to manage as part of the class
+# @param default_package_action [String] Default value of ensure if not specified for a package
+class spectrum_protect_client (
+  Hash[String,Hash] $packages,
+  Enum['latest','installed','absent'] $default_package_action,
+) {
+  $packages.each | $package,$params | {
+    $_params = { 'ensure' => $default_package_action } + $params
+    package { $package:
+      * => $_params,
+    }
+  }
 }
