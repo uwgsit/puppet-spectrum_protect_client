e605ce1fef2fa77fc2bf8b9f45111f70fcb5d1e4
Skylar Thompson <skylar2@uw.edu>
2023-03-06 11:11:17 -0800

    commit class re #1

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include spectrum_protect_client
+class spectrum_protect_client {
+}
