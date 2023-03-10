6fee2cb4f54b8a048c39239b572118f99841754e
Skylar Thompson <skylar2@uw.edu>
2023-03-10 09:14:28 -0800

    versionlocking test cases re #2

--- spec/classes/spectrum_protect_client_spec.rb
+++ spec/classes/spectrum_protect_client_spec.rb
@@ -5,10 +5,11 @@ require 'spec_helper'
 describe 'spectrum_protect_client' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      version = '8.1.15'
       let(:facts) { os_facts }
       let(:params) {
         {
-          'version' => '8.1.15',
+          'version' => version,
         }
       }
 
@@ -16,6 +17,9 @@ describe 'spectrum_protect_client' do
 
       it { is_expected.to contain_package('TIVsm-BA') }
       it { is_expected.to contain_package('TIVsm-API64') }
+
+      it { is_expected.to contain_yum__versionlock("0:TIVsm-BA-#{version}") }
+      it { is_expected.to contain_yum__versionlock("0:TIVsm-API64-#{version}") }
 
     end
   end
