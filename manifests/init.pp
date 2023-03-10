f577e164e2307e11eb286160cd13872309c90896
Skylar Thompson <skylar2@uw.edu>
2023-03-10 09:40:27 -0800

    produce package_requires array re #1

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,15 @@ class spectrum_protect_client (
   String $version,
   Enum['latest','installed','absent'] $default_package_action,
 ) {
-  $packages.each | $package,$params | {
-    $_params = { 'ensure' => $default_package_action } + $params
+  $package_requires = $packages.reduce([]) | $package_requires,$package | {
+    $package_name = $package[0]
+    $package_params = { 'ensure'  => $default_package_action } + $package[1]
     # Upgrading BA packages should only be done when needed
     yum::versionlock { "0:${package}-${version}":
       ensure => present,
     } -> package { $package:
-      * => $_params,
+      * => $package_params,
     }
+    $package_requires + Package[$package_name]
   }
 }
